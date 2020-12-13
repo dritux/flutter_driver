e350a79c1a3b611ce9fb75eaa1e33bea36b068a9
Adriano Fialho <dritec@gmail.com>
2020-12-12 21:53:35 -0300

    Update

---
+++ BrowserStackSample.js
@@ -0,0 +1,68 @@
+let wd = require('wd');
+let assert = require('assert');
+let asserters = wd.asserters;
+
+desiredCaps = {
+  // Set your BrowserStack access credentials
+  'browserstack.user' : 'automaovesti1',
+  'browserstack.key' : 'HYamkp5sBqpXNh1zFz9s',
+
+  // Set URL of the application under test
+  'app' : 'bs://063ceb58b679b856452f36223d31199276f0b469',
+
+  // Specify device and os_version for testing
+  'device' : 'Google Pixel 3',
+  'os_version' : '9.0',
+
+  // Set other BrowserStack capabilities
+  'project' : 'First NodeJS project',
+  'build' : 'Node Android',
+  'name': 'first_test',
+  'browserstack.networkLogs': 'true',
+  'browserstack.deviceLogs': 'true',
+  'browserstack.appiumLogs': 'true',
+};
+
+
+
+
+
+
+// Initialize the remote Webdriver using BrowserStack remote URL
+// and desired capabilities defined above
+driver = wd.promiseRemote("http://hub-cloud.browserstack.com/wd/hub");
+
+
+
+(async () => {
+
+    describe('Search Wikipedia Functionality', () => {
+
+        it('can find search results', async () => {
+          const counterTextFinder = find.byValueKey('counter');
+          const buttonFinder = find.byValueKey('increment');
+
+            assert.strictEqual(await driver.getElementText(counterTextFinder), '0');
+            await driver.elementClick(buttonFinder);
+            await driver.touchAction({
+                action: 'tap',
+                element: { elementId: buttonFinder }
+            });
+
+            assert.strictEqual(await driver.getElementText(counterTextFinder), '1');
+        });
+    });
+
+
+  });
+
+
+
+
+driver
+  .init(desiredCaps)
+  //  Write your code here
+  .fin(function() {
+return driver.quit(); })
+  .done();
+
--- bs.config.js
+++ bs.config.js
@@ -1,27 +1,22 @@
 exports.config = {
-  user: process.env.BROWSERSTACK_USERNAME || 'automaovesti1',
-  key: process.env.BROWSERSTACK_ACCESS_KEY || 'HYamkp5sBqpXNh1zFz9s',
-
     commonCapabilities: {
       project: "First Webdriverio Android Project",
       build: 'Webdriverio Android Parallel',
       name: 'parallel_test',
-      app: process.env.BROWSERSTACK_APP_ID || 'app_url":"bs://242a2495820a8bf7ed39eab8ed37e935fd1c87e0"}',
+      app: process.env.BROWSERSTACK_APP_ID || 'app_url":"bs://063ceb58b679b856452f36223d31199276f0b469"}',
       'browserstack.debug': true
     },
-
-    capabilities: [{
-      device: 'Google Pixel 3',
-      os_version: "9.0"
-    }, {
-      device: 'Samsung Galaxy S10e',
-      os_version: "9.0"
-    }],
-
+    capabilities: [
+        {
+          device: 'Google Pixel 3',
+          os_version: "9.0"
+        }, {
+          device: 'Samsung Galaxy S10e',
+          os_version: "9.0"
+        }
+    ],
     //...
   };
-
-  // Code to support common capabilities
   exports.config.capabilities.forEach(function(caps) {
     for(var i in exports.config.commonCapabilities) {
       caps[i] = caps[i] || exports.config.commonCapabilities[i];
